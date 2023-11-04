e3123824bc53040f3f5e43beea1684758714c9e6
morote21 <josemg00021@gmail.com>
2023-11-04 02:36:54 +0100

    inicio de gui hecha: boton para insertar topview image y coger coordenadas clickeando y soltando click

--- presentation_layer/gui.py
+++ presentation_layer/gui.py
@@ -1,25 +1,79 @@
-import tkinter
-import tkinter as tk
-from tkinter import filedialog
+import os
+import sys
+import cv2
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PIL import Image
+os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
 
 
-def UploadAction(event=None):
-    filename = filedialog.askopenfilename()
-    print("Selected:", filename)
+class TopviewWindow(QWidget):
+    def __init__(self, path):
+        super().__init__()
+
+        self.image_label = QLabel(self)
+        self.pixmap = QPixmap(path)
+        self.image_label.setPixmap(self.pixmap)
+        self.resize(self.pixmap.width(),
+                    self.pixmap.height())
+
+        self.image_label.mousePressEvent = self.get_coords_init
+        self.image_label.mouseReleaseEvent = self.get_coords_end
+
+        self.show()
+
+    def get_coords_init(self, event):
+        x = event.pos().x()
+        y = event.pos().y()
+        print(x, y)
+
+    def get_coords_end(self, event):
+        x = event.pos().x()
+        y = event.pos().y()
+        print(x, y)
 
 
-window = tk.Tk()
+class MainApp(QMainWindow):
+    def __init__(self):
+        super(MainApp, self).__init__()
+        self.topview_image = None
+        self.video = None
+        self.tw_window = None
 
-window_width = window.winfo_screenwidth()
-window_height = window.winfo_screenheight()
+        self.setWindowTitle("Statistics Generator")
 
-window.geometry("%dx%d" % (window_width, window_height))
-window.title("Statistics generator")
+        layout = QVBoxLayout()
 
-# label = tk.Label(window, text="Statistics generator")
-# label.pack()
+        button = QPushButton("Upload Image")
+        button.setCheckable(False)
+        button.clicked.connect(self.load_topview)
+
+        layout.addWidget(QLabel("Insert top-view image:"))
+        layout.addWidget(button)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def the_button_was_clicked(self):
+        print("The button was clicked")
+
+    def load_topview(self):
+        options = QFileDialog.Options()
+        topview_path, _ = QFileDialog.getOpenFileName(self, "Open Video File", "", options=options)
+
+        if topview_path is not None:
+            self.tw_window = TopviewWindow(topview_path)
+            self.tw_window.show()
 
 
-button = tk.Button(window, text="Open Video", command=UploadAction)
-button.place(x=0, y=0)
-window.mainloop()
+def main():
+    app = QApplication([])  # Initialize app
+    main_window = MainApp()  # Main window stablished
+    main_window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
